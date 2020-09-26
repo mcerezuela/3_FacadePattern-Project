65e07487e009298e08f114b39808a5cd244695fc
marcusalten <marcus.cerezuela@alten.es>
2020-09-26 17:20:20 +0200

    Added Facade Pattern

---
+++ FacadePattern/test/com/smashtik/patterns/facadepattern/ScheduleServerTest.java
@@ -0,0 +1,62 @@
+package com.smashtik.patterns.facadepattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleServerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void startBooting() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readSystemConfigFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void init() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void initializeContext() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void initializeListeners() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createSystemObjects() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void releaseProcesses() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void destory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void destroySystemObjects() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void destoryListeners() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void destoryContext() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void shutdown() {
+    }
+}
